43b6af565066ca28c3f8cfda43c619a51e2008bc
2023bit505 <2023bit505@sggs.ac.in>
2024-01-05 08:32:01 +0530

    addition function

---
+++ additionfunction.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class additionfunction{
+    public static int add (int a, int b){
+        return a+b;
+    }
+    public static void main(String[] args) {
+        int result =add(5,7);
+        System.out.println("sum:"+result);
+    }
+}
