5b2dff78bfd9db058b268ee1087a9b9456fa173f
2023bit505 <2023bit505@sggs.ac.in>
2024-01-07 05:34:35 +0530

    sub function

---
+++ subtractionfunction.java
@@ -0,0 +1,15 @@
+public class subtractionfunction 
+{
+    public static int sub(int a, int b)
+    {
+        return a - b;
+    }
+
+    public static void main(String[] args) 
+    {
+        int a=9;
+        int b=4;
+        int result=sub(a, b);
+        System.out.println("the subtraction is "+result);
+    }
+}
