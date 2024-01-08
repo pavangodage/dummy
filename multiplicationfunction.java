6dd346ba4aecddf769cf4d2d2f85cee88e689be5
2023bit505 <2023bit505@sggs.ac.in>
2024-01-08 11:38:43 +0530

    mul function

---
+++ multiplicationfunction.java
@@ -0,0 +1,14 @@
+public class multiplicationfunction 
+{
+    public static int mul(int a, int b)
+    {
+        return a*b;
+    }
+    public static void main(String[] args) 
+    {
+        int a=4;
+        int b=4;
+        int result=mul(a, b);
+        System.out.println("multiplication is :"+result);
+    }
+}
