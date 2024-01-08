9b6c44a5eee63152203ebde6958799a75943b49c
2023bit505 <2023bit505@sggs.ac.in>
2024-01-09 04:41:10 +0530

    divide function

---
+++ dividefunction.java
@@ -0,0 +1,14 @@
+public class dividefunction 
+{
+    public static int divide(int a,int b)
+    {
+        return a/b;
+    }
+    public static void main(String[] args)
+    {
+        int a=16;
+        int b=4;
+        int result=divide(a, b);
+        System.out.println("the dividation is :"+result);
+    }
+}
