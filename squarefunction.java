008feec041d2bf21b050d51623a9a524e1f60f7c
2023bit505 <2023bit505@sggs.ac.in>
2024-01-11 10:01:45 +0530

    square functio

---
+++ squarefunction.java
@@ -0,0 +1,14 @@
+class squarefunction 
+{
+    public static int square(int a)
+    {
+        return (a*a);
+    }
+    public static void main(String[] args)
+    {
+        int a=8;
+        int result=square(a);
+        System.out.println("the square of 8 is :"+result);        
+    }
+    
+}
