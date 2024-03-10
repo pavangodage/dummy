d8eaf783702421c34c438bf05396a38f53d414ab
2023bit505 <2023bit505@sggs.ac.in>
2024-03-10 06:01:48 +0530

    varargs

---
+++ ex2varargs.java
@@ -0,0 +1,12 @@
+class VarargsExample1{  
+   
+    static void display(String... values){  
+     System.out.println("display method invoked ");  
+    }  
+     
+    public static void main(String args[]){  
+     
+    display();  
+    display("my","name","is","varargs");
+    }   
+   }  
