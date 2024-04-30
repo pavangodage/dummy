f115793919abfbd1458a2991d1aec5a6bb7b1e8d
2023bit505 <2023bit505@sggs.ac.in>
2024-04-30 10:34:26 +0530

    packages

---
+++ package.java
@@ -0,0 +1,8 @@
+
+import mypackage.MyPackageClass;
+public class MainClass {
+    public static void main(String[] args) {      
+        MyPackageClass myObj = new MyPackageClass();  
+        myObj.display();
+    }
+}
